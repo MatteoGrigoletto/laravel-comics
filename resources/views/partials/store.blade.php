b0ba2c6464f9dea69b5e27ed79a47ef27db04dbe
MatteoGrigoletto <110687743+MatteoGrigoletto@users.noreply.github.com>
2023-01-14 13:49:24 +0100

    Aggiunge shop

--- resources/views/partials/store.blade.php
+++ resources/views/partials/store.blade.php
@@ -1,3 +1,39 @@
 <section class="store">
-<h1>ciao</h1>
+<div class="my-logo">
+    <div>
+        <img class="" src="{{Vite::asset("resources/img/buy-comics-digital-comics.png")}}" alt="Logo DC">
+    </div>
+    <div>
+        <p><a href="#">DIGITAL COMICS</a></p>
+    </div>
+</div>
+<div class="my-logo">
+    <div>
+        <img class="" src="{{Vite::asset("resources/img/buy-comics-merchandise.png")}}" alt="Logo DC">
+    </div>
+    <div>
+    <p><a href="#">DIGITAL COMICS</a></p>
+    </div>
+</div><div class="my-logo">
+    <div>
+        <img class="" src="{{Vite::asset("resources/img/buy-comics-shop-locator.png")}}" alt="Logo DC">
+    </div>
+    <div>
+    <p><a href="#">DIGITAL COMICS</a></p>
+    </div>
+</div><div class="my-logo">
+    <div>
+        <img class="" src="{{Vite::asset("resources/img/buy-comics-subscriptions.png")}}" alt="Logo DC">
+    </div>
+    <div>
+    <p><a href="#">DIGITAL COMICS</a></p>
+    </div>
+</div><div class="my-logo">
+    <div>
+        <img class="" src="{{Vite::asset("resources/img/buy-dc-power-visa.svg")}}" alt="Logo DC">
+    </div>
+    <div>
+    <p><a href="#">DIGITAL COMICS</a></p>
+    </div>
+</div>
 </section>
